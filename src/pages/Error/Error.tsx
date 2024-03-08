baee41b890eb2c1c05811a653cf62598d93d0f09
Kah Ho <sweekahho@gmail.com>
2024-03-08 17:02:00 +0800

    Add fallback page for invalid urls

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,7 +1,7 @@
 import { Flex, Text } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
-const Posts: React.FC = () => {
+const Error: React.FC = () => {
 	return (
 		<Flex
 			height="100vh"
@@ -9,11 +9,10 @@ const Posts: React.FC = () => {
 			alignItems="center"
 			paddingTop="40px"
 		>
-			<Text>Posts go here!!!!!</Text>
+			<Text>Invalid URL, go back to home: </Text>
 			<Link to="/">Home</Link>
-			<Link to="/posts">Posts</Link>
 		</Flex>
 	)
 }
 
-export default Posts
+export default Error
--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
 import Posts from './Posts/Posts'
+import Error from './Error/Error'
 
 const App: React.FC = () => (
 	<Router>
 		<Routes>
 			{/* Public Pages */}
-			<Route path="/*" element={<Home />} />
+			<Route path="/" element={<Home />} />
 			<Route path="/posts" element={<Posts />} />
+			<Route path="*" element={<Error />} />
 		</Routes>
 	</Router>
 )
