5f752d23662d4b9bc2f781f0eaa3475f48d50dac
Kah Ho <sweekahho@gmail.com>
2024-03-08 17:44:53 +0800

    Switch to hashrouter

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
 import Posts from './Posts/Posts'
 import Error from './Error/Error'
 
 const App: React.FC = () => (
-	<Router>
+	<HashRouter>
 		<Routes>
 			{/* Public Pages */}
 			<Route path="/" element={<Home />} />
 			<Route path="/posts" element={<Posts />} />
 			<Route path="/*" element={<Error />} />
 		</Routes>
-	</Router>
+	</HashRouter>
 )
 
 export default App
